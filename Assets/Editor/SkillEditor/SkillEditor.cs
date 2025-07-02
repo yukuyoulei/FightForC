c52bba006adec317765858b33a7afc7555b1a70a
wangshuo <63419182@163.com>
2025-07-02 16:26:35 +0800

    导出技能配置（依然Unity AI制造）

--- Assets/Editor/SkillEditor/SkillEditor.cs
+++ Assets/Editor/SkillEditor/SkillEditor.cs
@@ -1,11 +1,13 @@
 // 2025/7/2 AI-Tag
 // This was created with the help of Assistant, a Unity Artificial Intelligence product.
 
-using UnityEditor;
-using UnityEngine;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System;
+using UnityEditor;
+using UnityEngine;
+using static SkillEditor;
+using static Unity.Collections.AllocatorManager;
 
 public class SkillEditor : EditorWindow
 {
@@ -54,73 +56,44 @@ public class SkillEditor : EditorWindow
     {
         GUILayout.Label("Skill Editor", EditorStyles.boldLabel);
 
-        GUILayout.Label("Skill List", EditorStyles.boldLabel);
-        for (int i = 0; i < skills.Count; i++)
+        if (selectedSkillIndex == -1)
         {
-            GUILayout.BeginHorizontal();
-
-            if (GUILayout.Button(skills[i].skillName, GUILayout.Width(200)))
+            GUILayout.Label("Skill List", EditorStyles.boldLabel);
+            for (int i = 0; i < skills.Count; i++)
             {
-                selectedSkillIndex = i;
-            }
+                GUILayout.BeginHorizontal();
 
-            if (GUILayout.Button("Delete", GUILayout.Width(70)))
-            {
-                if (EditorUtility.DisplayDialog("Delete Skill", $"Are you sure you want to delete the skill '{skills[i].skillName}'?", "Yes", "No"))
+                if (GUILayout.Button(skills[i].skillName, GUILayout.Width(200)))
                 {
-                    skills.RemoveAt(i);
-                    if (selectedSkillIndex == i) selectedSkillIndex = -1;
-                    break;
+                    selectedSkillIndex = i;
                 }
-            }
 
-            GUILayout.EndHorizontal();
-        }
-
-        GUILayout.Space(10);
-
-        GUILayout.BeginHorizontal();
-        newSkillName = GUILayout.TextField(newSkillName, GUILayout.Width(200));
-        if (GUILayout.Button("Add Skill", GUILayout.Width(100)) && !string.IsNullOrEmpty(newSkillName))
-        {
-            skills.Add(new Skill { skillName = newSkillName });
-            newSkillName = "";
-        }
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(20);
-
-        if (selectedSkillIndex >= 0 && selectedSkillIndex < skills.Count)
-        {
-            GUILayout.Label($"Editing Skill: {skills[selectedSkillIndex].skillName}", EditorStyles.boldLabel);
-
-            if (GUILayout.Button("Add Skill Block"))
-            {
-                skills[selectedSkillIndex].skillBlocks.Add(new SkillBlock());
-            }
-
-            for (int i = 0; i < skills[selectedSkillIndex].skillBlocks.Count; i++)
-            {
-                GUILayout.BeginVertical("box");
-
-                GUILayout.Label($"Skill Block {i + 1}");
-
-                skills[selectedSkillIndex].skillBlocks[i].triggerMilliseconds = EditorGUILayout.IntField("Trigger Milliseconds", skills[selectedSkillIndex].skillBlocks[i].triggerMilliseconds);
-                skills[selectedSkillIndex].skillBlocks[i].effectType = (EffectType)EditorGUILayout.EnumPopup("Effect Type", skills[selectedSkillIndex].skillBlocks[i].effectType);
-                skills[selectedSkillIndex].skillBlocks[i].effectValue = EditorGUILayout.FloatField("Effect Value", skills[selectedSkillIndex].skillBlocks[i].effectValue);
-                skills[selectedSkillIndex].skillBlocks[i].effectString = EditorGUILayout.TextField("Effect String", skills[selectedSkillIndex].skillBlocks[i].effectString);
-
-                if (GUILayout.Button("Remove Skill Block"))
+                if (GUILayout.Button("Delete", GUILayout.Width(70)))
                 {
-                    if (EditorUtility.DisplayDialog("Remove Skill Block", $"Are you sure you want to remove Skill Block {i + 1}?", "Yes", "No"))
+                    if (EditorUtility.DisplayDialog("Delete Skill", $"Are you sure you want to delete the skill '{skills[i].skillName}'?", "Yes", "No"))
                     {
-                        skills[selectedSkillIndex].skillBlocks.RemoveAt(i);
+                        skills.RemoveAt(i);
                         break;
                     }
                 }
 
-                GUILayout.EndVertical();
+                GUILayout.EndHorizontal();
             }
+
+            GUILayout.Space(10);
+
+            GUILayout.BeginHorizontal();
+            newSkillName = GUILayout.TextField(newSkillName, GUILayout.Width(200));
+            if (GUILayout.Button("Add Skill", GUILayout.Width(100)) && !string.IsNullOrEmpty(newSkillName))
+            {
+                skills.Add(new Skill { skillName = newSkillName });
+                newSkillName = "";
+            }
+            GUILayout.EndHorizontal();
+        }
+        else
+        {
+            RenderSkillEditor();
         }
 
         GUILayout.Space(20);
@@ -133,6 +106,45 @@ public class SkillEditor : EditorWindow
         if (GUILayout.Button("Export Skills to Code"))
         {
             ExportSkillsToCode();
+        }
+    }
+
+    private void RenderSkillEditor()
+    {
+        GUILayout.Label($"Editing Skill: {skills[selectedSkillIndex].skillName}", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Back to Skill List"))
+        {
+            selectedSkillIndex = -1;
+            return;
+        }
+
+        if (GUILayout.Button("Add Skill Block"))
+        {
+            skills[selectedSkillIndex].skillBlocks.Add(new SkillBlock());
+        }
+
+        for (int i = 0; i < skills[selectedSkillIndex].skillBlocks.Count; i++)
+        {
+            GUILayout.BeginVertical("box");
+
+            GUILayout.Label($"Skill Block {i + 1}");
+
+            skills[selectedSkillIndex].skillBlocks[i].triggerMilliseconds = EditorGUILayout.IntField("Trigger Milliseconds", skills[selectedSkillIndex].skillBlocks[i].triggerMilliseconds);
+            skills[selectedSkillIndex].skillBlocks[i].effectType = (EffectType)EditorGUILayout.EnumPopup("Effect Type", skills[selectedSkillIndex].skillBlocks[i].effectType);
+            skills[selectedSkillIndex].skillBlocks[i].effectValue = EditorGUILayout.FloatField("Effect Value", skills[selectedSkillIndex].skillBlocks[i].effectValue);
+            skills[selectedSkillIndex].skillBlocks[i].effectString = EditorGUILayout.TextField("Effect String", skills[selectedSkillIndex].skillBlocks[i].effectString);
+
+            if (GUILayout.Button("Remove Skill Block"))
+            {
+                if (EditorUtility.DisplayDialog("Remove Skill Block", $"Are you sure you want to remove Skill Block {i + 1}?", "Yes", "No"))
+                {
+                    skills[selectedSkillIndex].skillBlocks.RemoveAt(i);
+                    break;
+                }
+            }
+
+            GUILayout.EndVertical();
         }
     }
 
@@ -152,7 +164,7 @@ public class SkillEditor : EditorWindow
 
     private void ExportSkillsToCode()
     {
-        string exportPath = Path.Combine(Application.dataPath, "BaseScripts/ConfigSkill/Config_Skills.cs");
+        string exportPath = Path.Combine(Application.dataPath, "../ConfigSkill/Config_Skills.cs");
         try
         {
             using (StreamWriter writer = new StreamWriter(exportPath))
