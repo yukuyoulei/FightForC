c7d0eab3c2971d64457acfb095b0be112d4b0ed0
ws <63419182@163.com>
2025-11-22 23:05:32 +0800

    技能编辑器修改为

--- Assets/Editor/SkillEditor/SkillEditor.cs
+++ Assets/Editor/SkillEditor/SkillEditor.cs
@@ -2,8 +2,10 @@
 // This was created with the help of Assistant, a Unity Artificial Intelligence product.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 using static SkillEditor;
@@ -14,7 +16,18 @@ public class SkillEditor : EditorWindow
     [System.Serializable]
     public class Skill
     {
+        /// <summary>
+        /// 技能名称
+        /// </summary>
         public string skillName;
+        /// <summary>
+        /// CD时间
+        /// </summary>
+        public float cd;
+        /// <summary>
+        /// 技能释放半径
+        /// </summary>
+        public float radius;
         public List<SkillBlock> skillBlocks = new List<SkillBlock>();
     }
 
@@ -119,33 +132,11 @@ public class SkillEditor : EditorWindow
             return;
         }
 
-        if (GUILayout.Button("Add Skill Block"))
-        {
-            skills[selectedSkillIndex].skillBlocks.Add(new SkillBlock());
-        }
-
-        for (int i = 0; i < skills[selectedSkillIndex].skillBlocks.Count; i++)
-        {
-            GUILayout.BeginVertical("box");
-
-            GUILayout.Label($"Skill Block {i + 1}");
-
-            skills[selectedSkillIndex].skillBlocks[i].triggerMilliseconds = EditorGUILayout.IntField("Trigger Milliseconds", skills[selectedSkillIndex].skillBlocks[i].triggerMilliseconds);
-            skills[selectedSkillIndex].skillBlocks[i].effectType = (EffectType)EditorGUILayout.EnumPopup("Effect Type", skills[selectedSkillIndex].skillBlocks[i].effectType);
-            skills[selectedSkillIndex].skillBlocks[i].effectValue = EditorGUILayout.FloatField("Effect Value", skills[selectedSkillIndex].skillBlocks[i].effectValue);
-            skills[selectedSkillIndex].skillBlocks[i].effectString = EditorGUILayout.TextField("Effect String", skills[selectedSkillIndex].skillBlocks[i].effectString);
-
-            if (GUILayout.Button("Remove Skill Block"))
-            {
-                if (EditorUtility.DisplayDialog("Remove Skill Block", $"Are you sure you want to remove Skill Block {i + 1}?", "Yes", "No"))
-                {
-                    skills[selectedSkillIndex].skillBlocks.RemoveAt(i);
-                    break;
-                }
-            }
-
-            GUILayout.EndVertical();
-        }
+        // 使用反射渲染技能的所有字段
+        GUILayout.BeginVertical("box");
+        GUILayout.Label("Skill Properties", EditorStyles.boldLabel);
+        RenderObjectFields(skills[selectedSkillIndex]);
+        GUILayout.EndVertical();
     }
 
     private void SaveSkills()
@@ -234,6 +225,156 @@ public class SkillEditor : EditorWindow
         else
         {
             Debug.Log("No save file found, starting with an empty skill list.");
+        }
+    }
+
+    /// <summary>
+    /// 通过反射渲染对象的所有字段
+    /// </summary>
+    /// <param name="obj">要渲染的对象</param>
+    private void RenderObjectFields(object obj)
+    {
+        if (obj == null) return;
+
+        Type objType = obj.GetType();
+        FieldInfo[] fields = objType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (FieldInfo field in fields)
+        {
+            string fieldName = field.Name;
+            object fieldValue = field.GetValue(obj);
+            Type fieldType = field.FieldType;
+
+            try
+            {
+                if (fieldType == typeof(int))
+                {
+                    int intValue = EditorGUILayout.IntField(fieldName, (int)fieldValue);
+                    field.SetValue(obj, intValue);
+                }
+                else if (fieldType == typeof(float))
+                {
+                    float floatValue = EditorGUILayout.FloatField(fieldName, (float)fieldValue);
+                    field.SetValue(obj, floatValue);
+                }
+                else if (fieldType == typeof(string))
+                {
+                    string stringValue = EditorGUILayout.TextField(fieldName, (string)fieldValue);
+                    field.SetValue(obj, stringValue);
+                }
+                else if (fieldType == typeof(bool))
+                {
+                    bool boolValue = EditorGUILayout.Toggle(fieldName, (bool)fieldValue);
+                    field.SetValue(obj, boolValue);
+                }
+                else if (fieldType.IsEnum)
+                {
+                    Enum enumValue = EditorGUILayout.EnumPopup(fieldName, (Enum)fieldValue);
+                    field.SetValue(obj, enumValue);
+                }
+                else if (fieldType == typeof(Vector2))
+                {
+                    Vector2 vector2Value = EditorGUILayout.Vector2Field(fieldName, (Vector2)fieldValue);
+                    field.SetValue(obj, vector2Value);
+                }
+                else if (fieldType == typeof(Vector3))
+                {
+                    Vector3 vector3Value = EditorGUILayout.Vector3Field(fieldName, (Vector3)fieldValue);
+                    field.SetValue(obj, vector3Value);
+                }
+                else if (fieldType == typeof(Vector4))
+                {
+                    Vector4 vector4Value = EditorGUILayout.Vector4Field(fieldName, (Vector4)fieldValue);
+                    field.SetValue(obj, vector4Value);
+                }
+                else if (fieldType == typeof(Color))
+                {
+                    Color colorValue = EditorGUILayout.ColorField(fieldName, (Color)fieldValue);
+                    field.SetValue(obj, colorValue);
+                }
+                else if (typeof(IList).IsAssignableFrom(fieldType) && fieldType.IsGenericType)
+                {
+                    // 处理List<T>类型
+                    Type elementType = fieldType.GetGenericArguments()[0];
+                    IList list = (IList)fieldValue;
+                    
+                    GUILayout.BeginVertical("box");
+                    GUILayout.Label($"{fieldName} (List<{elementType.Name}>)", EditorStyles.boldLabel);
+                    
+                    int newCount = EditorGUILayout.IntField("Size", list.Count);
+                    if (newCount != list.Count)
+                    {
+                        // 调整列表大小
+                        while (list.Count < newCount)
+                        {
+                            list.Add(Activator.CreateInstance(elementType));
+                        }
+                        while (list.Count > newCount)
+                        {
+                            list.RemoveAt(list.Count - 1);
+                        }
+                    }
+                    
+                    // 渲染列表中的每个元素
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        GUILayout.BeginVertical("box");
+                        GUILayout.Label($"Element {i + 1}");
+                        
+                        object element = list[i];
+                        if (element != null && !elementType.IsPrimitive && elementType != typeof(string) && !elementType.IsEnum)
+                        {
+                            // 对于复杂类型，递归渲染其字段
+                            RenderObjectFields(element);
+                        }
+                        else
+                        {
+                            // 对于简单类型，直接渲染
+                            if (elementType == typeof(int))
+                            {
+                                list[i] = EditorGUILayout.IntField("Value", (int)element);
+                            }
+                            else if (elementType == typeof(float))
+                            {
+                                list[i] = EditorGUILayout.FloatField("Value", (float)element);
+                            }
+                            else if (elementType == typeof(string))
+                            {
+                                list[i] = EditorGUILayout.TextField("Value", (string)element);
+                            }
+                            else if (elementType == typeof(bool))
+                            {
+                                list[i] = EditorGUILayout.Toggle("Value", (bool)element);
+                            }
+                            else if (elementType.IsEnum)
+                            {
+                                list[i] = EditorGUILayout.EnumPopup("Value", (Enum)element);
+                            }
+                        }
+                        
+                        GUILayout.EndVertical();
+                    }
+                    
+                    GUILayout.EndVertical();
+                }
+                else if (!fieldType.IsPrimitive && fieldType != typeof(string) && !fieldType.IsEnum)
+                {
+                    // 对于其他复杂类型，递归渲染
+                    GUILayout.BeginVertical("box");
+                    GUILayout.Label($"{fieldName} ({fieldType.Name})", EditorStyles.boldLabel);
+                    RenderObjectFields(fieldValue);
+                    GUILayout.EndVertical();
+                }
+                else
+                {
+                    // 对于不支持的类型，显示只读字段
+                    EditorGUILayout.LabelField(fieldName, fieldValue?.ToString() ?? "null");
+                }
+            }
+            catch (Exception e)
+            {
+                EditorGUILayout.LabelField(fieldName, $"Error: {e.Message}");
+            }
         }
     }
 
